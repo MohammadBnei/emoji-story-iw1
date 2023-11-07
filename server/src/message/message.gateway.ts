564227e3dd8ef9e43f1d362b33dd80a90f35a5e9
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-11-07 10:39:08 +0100

    first commit

---
+++ server/src/message/message.gateway.ts
@@ -0,0 +1,99 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+
+// Step ->
+//          Choix d'emojis proposé
+//          Vote en temps réel
+//          Après un temps imparti, l'emoji qui a le plus de votes est enregistré
+//          -> Cycle
+
+interface StoryStep {
+  selectedEmoji: string;
+  order: number;
+  emojiCandidate: Record<string, number>;
+}
+
+interface Story {
+  storyGPT?: string;
+  steps: StoryStep[];
+}
+
+@WebSocketGateway()
+export class MessageGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Socket;
+
+  clientList: Map<Socket, Record<number, string>> = new Map();
+
+  story: Story = {
+    storyGPT: '',
+    steps: [
+      {
+        selectedEmoji: '',
+        order: 1,
+        emojiCandidate: { '😞': 0, '🤜🏽': 0, '🧌': 0, ඞ: 0 },
+      },
+    ],
+  };
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.clientList.set(client, {});
+
+    client.emit('story-update', this.story);
+  }
+
+  handleDisconnect(client: any, ...args: any[]) {
+    this.clientList.delete(client);
+  }
+
+  /**
+   * Gerer le vote
+   * Créer une fonction avec un subscriber
+   * - Augmenter le nombre de vote de l'emoji selectionné sur le current step ✅
+   * - Synchroniser tous les clients ✅
+   * - Gérer les erreurs ✅
+   *
+   * step-vote
+   */
+
+  @SubscribeMessage('step-vote')
+  stepVote(
+    client: Socket,
+    payload: {
+      emoji: string;
+      stepOrder: number;
+    },
+  ) {
+    try {
+      const currentStep = this.story.steps.at(-1);
+      const currentStepIndex = this.story.steps.length;
+      // No step
+      // emoji not found
+      if (this.story.steps.length === 0)
+        throw new Error("The vote hasn't started");
+      if (!Object.keys(currentStep.emojiCandidate).includes(payload.emoji))
+        throw new Error("You voted for an emoji that isn't a candidate?!");
+
+      const clientVote = this.clientList.get(client);
+
+      if (clientVote[currentStepIndex] === payload.emoji)
+        throw new Error(`You already voted for : ${payload.emoji}`);
+
+      clientVote[currentStepIndex] = payload.emoji;
+
+      currentStep.emojiCandidate[payload.emoji]++;
+
+      this.server.emit('story-update', this.story);
+    } catch (err) {
+      this.server.emit('story-error', err.message);
+    }
+  }
+}
